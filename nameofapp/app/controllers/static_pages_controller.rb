e2cf84e2b1ce8003d5d84bd5a5e92377eb77a38c
King-of-Code <King5ofCode5@gmail.com>
2016-06-25 19:58:20 -0700

    fixed footer for all pages

--- nameofapp/app/controllers/static_pages_controller.rb
+++ nameofapp/app/controllers/static_pages_controller.rb
@@ -6,13 +6,10 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(3)
   end
 
-  def thank_you
+	def thank_you
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  ActionMailer::Base.mail(:from => @email,
-	      :to => "slxskytiger@yahoo.com",
-	      :subject => "A new contact form message from #{@name}",
-	      :body => @message).deliver_now
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 end
