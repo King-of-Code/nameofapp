06b33059e7914d3a8124919f5f9a2aa389230988
King-of-Code <King5ofCode5@gmail.com>
2016-08-01 23:48:30 -0700

    added jquery plugins

--- nameofapp/app/assets/javascripts/site.js
+++ nameofapp/app/assets/javascripts/site.js
@@ -1,4 +1,5 @@
 $(document).on('ready page:load', function(){
+	console.log( "document loaded" );
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
     readOnly: true,
@@ -6,4 +7,18 @@ $(document).on('ready page:load', function(){
       return $(this).attr('data-score');
     }
    });
+
+	$('#wrapper').tubular({
+		videoId: 'igSkVuxMRsw'
+
+	}); // where idOfYourVideo is the YouTube ID. });
+
+	$('.img-zoom').elevateZoom({
+		scrollZoom : true,
+		zoomWindowFadeIn: 500,
+		zoomWindowFadeOut: 500,
+		lensFadeIn: 500,
+		lensFadeOut: 500
+	});
+
 });
