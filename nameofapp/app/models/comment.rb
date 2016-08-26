359beaaa48bf5f11bac8cf5ea43ee96972ba5c98
King-of-Code <sLxskytiger@yahoo.com>
2016-08-26 16:29:20 -0700

    fixing admin privelages

--- nameofapp/app/models/comment.rb
+++ nameofapp/app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
   validates :body, presence: true
   validates :user, presence: true	 
   validates :product, presence: true
-	validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { only_integer: true }
   validates :user_id, :uniqueness => { :scope => :product_id, :message => "Already commented on product"}
   after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
