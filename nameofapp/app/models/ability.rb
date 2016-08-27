dc082a8969966531e222e4979f2aae333f60ed71
King-of-Code <sLxskytiger@yahoo.com>
2016-08-27 16:52:11 -0700

    fixing admin privelages

--- nameofapp/app/models/ability.rb
+++ nameofapp/app/models/ability.rb
@@ -6,8 +6,8 @@ class Ability
     can :manage, User, id: user.id
     if user.admin?
         can :manage, :all
-    #else
-        #can :read, :all
+    else
+        can :read, :all
     end
     # Define abilities for the passed in user here. For example:
     #
