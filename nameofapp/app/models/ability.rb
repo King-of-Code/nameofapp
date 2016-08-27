c0553628ef378dd75bb37aecc6994690d23313dd
King-of-Code <sLxskytiger@yahoo.com>
2016-08-27 16:33:25 -0700

    fixing admin privelages

--- nameofapp/app/models/ability.rb
+++ nameofapp/app/models/ability.rb
@@ -2,12 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    #user ||= User.new # guest user (not logged in)
-    #can :manage, User, id: user.id
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
     if user.admin?
         can :manage, :all
-    else
-        can :read, :all
+    #else
+        #can :read, :all
     end
     # Define abilities for the passed in user here. For example:
     #
